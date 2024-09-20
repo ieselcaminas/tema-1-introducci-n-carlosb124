dba04ccdb7b857b6780ff4ea962cf1c57c88c95e
carlos124 <carbuc2@alu.edu.gva.es>
2024-09-20 11:41:15 +0200

    While

---
+++ src/Ejerciciowhile.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Ejerciciowhile {
+    import java.util.Scanner;
+
+    public class EjercicioWhile {
+        public static void main(String[] args) {
+            int num;
+            int positives = 0;
+
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Dime 10 números y te diré cuáles son positivos");
+            num = sc.nextInt();
+
+
+            for (int contador = 0; contador < 10; contador++) {
+                num = sc.nextInt();
+                if (num >= 0) {
+                    positives = positives + 1;
+
+                }
+            }
+
+            System.out.println("Tu numero de positivos es " + positives);
+        }
+    }
+}
